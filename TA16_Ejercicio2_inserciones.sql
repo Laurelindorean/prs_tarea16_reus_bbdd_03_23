4016a369403a13dd84ed444d67ca4d6821c722e9
Palmira Romia <100615218+Laurelindorean@users.noreply.github.com>
2023-03-27 23:18:44 +0200

    Inserciones a las tablas Departamentos y Empleados

---
+++ TA16_Ejercicio2_inserciones.sql
@@ -0,0 +1,24 @@
+USE actividades;
+INSERT INTO departamentos VALUES
+(1, 'Administración', 67000),
+(2, 'Dirección', 120000),
+(3, 'Logística', 25000),
+(4, 'Operaciones', 35000),
+(5, 'Compras', 63000),
+(6, 'Marketing', 45000),
+(7, 'Auditoria', 43002),
+(8, 'Tesorería', 84000),
+(9, 'Comercial', 57800),
+(10, 'Comunicación', 21040);
+
+INSERT INTO empleados VALUES
+(124578957, 'Palmira', 'Romia', 2),
+(457889986, 'Marc', 'Saez', 59),
+(157789963, 'Marc', 'Rojo', 9),
+(788456235, 'Francisco', 'Parra', 7),
+(854487995, 'Aitor', 'Iglesias', 14),
+(147845877, 'Elena', 'Lopez', 1),
+(658787896, 'Paula', 'Sousa', 5),
+(477711382, 'Alejandro', 'Jimenez', 3),
+(411228577, 'Camila', 'Garcia', 10),
+(488244279, 'Roger', 'Gibert', 77); 
