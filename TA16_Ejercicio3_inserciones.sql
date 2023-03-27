1418f7f8454e857bde3f26534ee51eabd93428fd
Palmira Romia <100615218+Laurelindorean@users.noreply.github.com>
2023-03-28 00:41:26 +0200

    Inserciones en las tablas Almacenes y Cajas

---
+++ TA16_Ejercicio3_inserciones.sql
@@ -0,0 +1,25 @@
+USE actividades;
+
+INSERT INTO almacenes VALUES
+(6, 'Lleida', 5),
+(7, 'Tarragona', 2),
+(8, 'Nueva York', 10),
+(9, 'Paris', 6),
+(10, 'Córdoba', 2),
+(11, 'Reus', 4),
+(12, 'Londres', 7),
+(13, 'Glasgow', 6),
+(14, 'Edimburgo', 4),
+(15, 'Tokio', 8);
+
+INSERT INTO cajas VALUES
+('1D5E', 'Patatas', 10, 7),
+('1R7T', 'Kiwis', 5, 2),
+('2V6R', 'Manzanas', 20, 2),
+('3M4N', 'Melocotones', 15, 6),
+('1T2Y', 'Peras', 4, 6),
+('8Z5L', 'Piñas', 25, 8),
+('2R3Y', 'Ciruelas', 30, 13),
+('7V8L', 'Cebollas', 14, 14),
+('8W7B', 'Ajos', 54, 9),
+('1F4T', 'Cerezas', 21, 6);
