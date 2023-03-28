062b4d1487d0b80e0c101d138b8564310aa6d7dd
Palmira Romia <100615218+Laurelindorean@users.noreply.github.com>
2023-03-29 00:41:22 +0200

    Inserciones a las tablas Salas y Peliculas

---
+++ TA16_Ejercicio4_inserciones.sql
@@ -0,0 +1,25 @@
+USE actividades;
+
+INSERT INTO peliculas VALUES
+(10, 'La Comunidad del Anillo', 12),
+(11, 'Matrix', 12),
+(12, 'It Follows', 16),
+(13, 'Los Goonies', 8),
+(14, 'Everything Everywhere All at Once', 14),
+(15, 'Malditos Bastardos', 16),
+(16, 'Saw', 18),
+(17, 'The Suicide Squad', 17),
+(18, 'Star Wars. Una nueva esperanza', 12),
+(19, 'Cadena perpetua', 14);
+
+INSERT INTO salas VALUES
+(7, 'Lauren', 10),
+(8, 'OCine', 15), 
+(9, 'Yelmo', 14),
+(10, 'JCA', 12),
+(11, 'Paradiso', NULL),
+(12, 'La Maquinista', 19),
+(13, '3D', 18),
+(14, 'Cinesa', 17),
+(15, 'Major', NULL),
+(16, 'Central', 11);
